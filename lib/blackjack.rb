ecfdfe1d909c1ee0107d9a8665d3b8270b3f4279
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-24 19:52:09 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -60,7 +60,7 @@ end
 # get every test to pass before coding runner below #
 #####################################################
 
-  def runner
+  def runner(run)
     # code runner here
     welcome
     initial_round
